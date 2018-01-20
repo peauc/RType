33da6ff06ec75d97e4f0737fd46ae11f4da5cae0
buis_p <pierre.buis@epitech.eu>
2018-01-20 23:29:44 +0000

    Enemies creation -> 1 enemy created

--- EnemyExample/inc/EnemyPhysicsComponent.hpp
+++ EnemyExample/inc/EnemyPhysicsComponent.hpp
@@ -3,6 +3,21 @@
 //
 
 #ifndef RTYPE_ENEMYPHYSICSCOMPONENT_HPP
-#define RTYPE_ENEMYPHYSICSCOMPONENT_HPP
+#	define RTYPE_ENEMYPHYSICSCOMPONENT_HPP
+
+#	include <Components/Abstracts/APhysicsComponent.hpp>
+
+namespace Component {
+	class EnemyPhysicsComponent : public APhysicsComponent
+	{
+	public:
+		EnemyPhysicsComponent(Engine::Entity *parentEntity, Engine::Hitbox hitbox);
+		~EnemyPhysicsComponent() override = default;
+
+		void update() override;
+
+		void cameraCollision(APhysicsComponent &other);
+	};
+}
 
 #endif //RTYPE_ENEMYPHYSICSCOMPONENT_HPP
--- EnemyExample/src/EnemyGraphicsComponent.cpp
+++ EnemyExample/src/EnemyGraphicsComponent.cpp
@@ -2,3 +2,42 @@
 // Created by buis_p on 1/18/18.
 //
 
+
+#include <iostream>
+#include "EnemyGraphicsComponent.hpp"
+
+Component::EnemyGraphicsComponent::EnemyGraphicsComponent(Engine::Entity *parentEntity,
+															RessourcesLoader *resourceLoader) :
+		AGraphicsComponent(parentEntity, resourceLoader)
+{
+	this->_animationIds.push_back(0);
+	this->_currentAnimationId = this->_animationIds[0];
+
+	this->_relativeStartPos.x = -500;
+	this->_relativeStartPos.y = -300;
+	this->_relativeRange.x = 1000;
+	this->_relativeRange.y = 600;
+
+	this->_validMessageTypes[Engine::Mediator::Message::DEATH] = std::bind(&EnemyGraphicsComponent::handleDeath,
+																		   this, std::placeholders::_1,
+																		   std::placeholders::_2);
+	this->_validMessageTypes[Engine::Mediator::Message::HIT] = std::bind(&EnemyGraphicsComponent::handleHit,
+																		 this, std::placeholders::_1,
+																		 std::placeholders::_2);
+}
+
+void Component::EnemyGraphicsComponent::update()
+{
+	std::cout << "Updating graphics" << std::endl;
+	this->sendToAll(Engine::Mediator::Message::GRAPHICS_REGISTERING);
+}
+
+void Component::EnemyGraphicsComponent::handleDeath(Engine::Mediator::Message, Engine::AComponent *)
+{
+	this->_isHit = true;
+}
+
+void Component::EnemyGraphicsComponent::handleHit(Engine::Mediator::Message, Engine::AComponent *)
+{
+	this->_isAlive = false;
+}
