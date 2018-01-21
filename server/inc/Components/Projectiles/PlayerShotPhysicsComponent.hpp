ae95b2914cd3680e4efa559b33eb399b093f309e
Romain samuel <romain.samuel@epitech.eu>
2018-01-21 12:47:52 +0100

    Added shot physics components

---
+++ server/inc/Components/Projectiles/PlayerShotPhysicsComponent.hpp
@@ -0,0 +1,21 @@
+//
+// Created by romain on 21/01/18.
+//
+
+#ifndef RTYPE_SHOTPHYSICSCOMPONENT_HPP
+#define RTYPE_SHOTPHYSICSCOMPONENT_HPP
+
+#include <Components/Abstracts/APhysicsComponent.hpp>
+
+namespace Component {
+	class PlayerShotPhysicsComponent : public APhysicsComponent
+	{
+	public:
+		PlayerShotPhysicsComponent(Engine::Entity *parentEntity, const Engine::Hitbox &hitbox);
+		~PlayerShotPhysicsComponent() override = default;
+
+		void update() noexcept override;
+	};
+}
+
+#endif //RTYPE_SHOTPHYSICSCOMPONENT_HPP
