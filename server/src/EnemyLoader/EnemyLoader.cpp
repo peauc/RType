e7ea6b4c8de99a5cc987944e3037a7b19f0a92ea
buis_p <pierre.buis@epitech.eu>
2018-01-21 17:41:09 +0000

    Enemies loader ---> main for tests
    Code optimisation+

--- server/src/EnemyLoader/EnemyLoader.cpp
+++ server/src/EnemyLoader/EnemyLoader.cpp
@@ -9,23 +9,30 @@
 EnemyLoader::EnemyLoader(const std::string &path, Engine::Game	&game) {
 	stock.setPath(path);
 	stock.loadByPath();
-	for (const std::string &file : stock.getList()) {
-		const std::string &str = path + file;
-		enemies.insert(std::pair<const std::string, Engine::Entity *>(file, loader.getInstance(str, game)));
+	if (stock.isFolder) {
+		for (const std::string &file : stock.getList()) {
+			const std::string &str = path + file;
+			enemies.insert(std::pair<const std::string, Engine::Entity *>(file, loader.getInstance(str, game)));
+		}
 	}
 }
 
-std::unique_ptr<std::map<const std::string, Engine::Entity*>> &EnemyLoader::getEnemies() const {
-	std::unique_ptr<std::map<const std::string, Engine::Entity*>>	ptr = std::make_unique<
-			std::map<const std::string, Engine::Entity*>>();
-	return (ptr);
+std::unique_ptr<std::map<const std::string, Engine::Entity*>> EnemyLoader::getEnemies() const {
+	if (!stock.isFolder) {
+		std::unique_ptr<std::map<const std::string, Engine::Entity *>> ptr = std::make_unique<
+				std::map<const std::string, Engine::Entity *>>(this->enemies);
+		return (std::move(ptr));
+	}
+	return (nullptr);
 }
 
 void EnemyLoader::setup(const std::string &path, Engine::Game &game) {
 	stock.setPath(path);
 	stock.loadByPath();
-	for (const std::string &file : stock.getList()) {
-		const std::string &str = path + file;
-		enemies.insert(std::pair<const std::string, Engine::Entity *>(file, loader.getInstance(str, game)));
+	if (stock.isFolder) {
+		for (const std::string &file : stock.getList()) {
+			const std::string &str = path + file;
+			enemies.insert(std::pair<const std::string, Engine::Entity *>(file, loader.getInstance(str, game)));
+		}
 	}
 }
