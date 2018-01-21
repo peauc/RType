09bf582b79c94205b1b718c979f807745a6cb767
Romain samuel <romain.samuel@epitech.eu>
2018-01-21 17:15:42 +0100

    main reset

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -13,34 +13,19 @@
 
 int main(int, char **)
 {
-//	try {
-//		AsioServer server;
-//		server.start();
-//		while (1) {
-//			server.tick();
-//		}
-//	} catch(boost::system::system_error& e)
-//	{
-//		std::cout<<"Error: " << e.what()<< std::endl;
-//		std::cout<<"Info: "  << boost::diagnostic_information(e)
-//		         << std::endl;
-//		return 1;
-//
-//	}
+	try {
+		AsioServer server;
+		server.start();
+		while (1) {
+			server.tick();
+		}
+	} catch(boost::system::system_error& e)
+	{
+		std::cout<<"Error: " << e.what()<< std::endl;
+		std::cout<<"Info: "  << boost::diagnostic_information(e)
+		         << std::endl;
+		return 1;
 
-	Engine::Game *game = new Engine::Game();
-	game->setup(1, nullptr);
-	Engine::Entity *player1 = Factory::EntityFactory::createPlayerShip(0, *game);
-	Engine::Entity *player2 = player1->clone();
-
-//	game->getWorld()->addObject(std::unique_ptr<Engine::Entity>(player1));
-//	game->getWorld()->addObject(std::unique_ptr<Engine::Entity>(player2));
-
-	game->getWorld()->update();
-
-//	std::shared_ptr<RessourcesLoader> rl = std::make_unique<RessourcesLoader>();
-//
-//	game->setup(1, rl);
-//	game->getWorld()->update();
+	}
 	return (0);
 }
