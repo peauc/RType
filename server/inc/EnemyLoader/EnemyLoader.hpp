f212cabd7c71bfdceaf3088eeb4f2070d6c80728
Lucas Villeneuve <lucas.villeneuve@epitech.eu>
2018-01-21 21:36:52 +0100

    Fix map

--- server/inc/EnemyLoader/EnemyLoader.hpp
+++ server/inc/EnemyLoader/EnemyLoader.hpp
@@ -6,8 +6,7 @@
 #	define RTYPE_ENEMYLOADER_HPP
 
 #	include <iostream>
-#	include <unordered_map>
-#include <map>
+#	include <map>
 #	include "Engine/Entity.hpp"
 #	include "EnemyLoader/LStock.hpp"
 #	include "EnemyLoader/DLLoader.hpp"
@@ -24,4 +23,5 @@ public:
 	std::unique_ptr<std::map<const std::string, Engine::Entity*>> getEnemies() const;
 	void setup(const std::string &path, Engine::Game &game);
 };
+
 #endif //RTYPE_ENEMYLOADER_HPP
