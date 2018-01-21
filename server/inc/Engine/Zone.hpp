fcbbcc2578cd1cd3838045491318eb32d6b57344
Romain samuel <romain.samuel@epitech.eu>
2018-01-21 18:34:06 +0100

    Zone

--- server/inc/Engine/Zone.hpp
+++ server/inc/Engine/Zone.hpp
@@ -16,18 +16,22 @@ namespace Engine {
 		class Zone
 		{
 		public:
-			Engine::Entity *createZone(Game &game) const;
+			Zone(const Vector2d &topLeftCoords, const Vector2d &bottomRightCoords);
+			~Zone() = default;
+			void createZone(Game &game) const;
 		private:
 			struct ZoneObject
 			{
 				const std::string _libname;
 				Vector2d _pos;
 
+				ZoneObject(const std::string &libname, const Vector2d &pos);
+				~ZoneObject() = default;
 				Engine::Entity *createEntity(Game &game) const;
 			};
 
-			Vector2d topLeftCoords;
-			Vector2d bottomRightCoords;
+			Vector2d _topLeftCoords;
+			Vector2d _bottomRightCoords;
 			std::vector<ZoneObject> _zoneObjects;
 		};
 	}
--- server/src/Engine/Zone.cpp
+++ server/src/Engine/Zone.cpp
@@ -5,8 +5,44 @@
 #include <Engine/Entity.hpp>
 #include <Engine/Game.hpp>
 #include <Engine/Zone.hpp>
+#include <Engine/Hitbox.hpp>
+#include <Components/Zone/ZoneComponent.hpp>
 
-Engine::Entity *Engine::MapLoader::Zone::createZone(Game &game) const
+Engine::MapLoader::Zone::Zone(const Vector2d &topLeftCoords, const Vector2d &bottomRightCoords)
+{
+	this->_topLeftCoords = topLeftCoords;
+	this->_bottomRightCoords = bottomRightCoords;
+}
+
+void Engine::MapLoader::Zone::createZone(Game &game) const
+{
+	Engine::Entity *zone = new Engine::Entity(0);
+
+	Component::ZoneComponent *zoneComponent = new Component::ZoneComponent(zone,
+																		   Engine::Hitbox(Engine::Hitbox::ZONE,
+																						  Vector2d(0, 0),
+																						  Vector2d(
+																								  this->_bottomRightCoords.x -
+																								  this->_topLeftCoords.x,
+																								  this->_bottomRightCoords.y -
+																								  this->_topLeftCoords.y)),
+																		   game.getWorld().get());
+
+	for (const auto &entity : this->_zoneObjects) {
+		zoneComponent->addEntity(std::unique_ptr<Entity>(entity.createEntity(game)));
+	}
+
+	zone->addComponent(zoneComponent);
+	if (game.getWorld()->getMediator() != nullptr) {
+		zoneComponent->registerToMediator(game.getWorld()->getMediator().get());
+	}
+	zone->setActive(true);
+
+	game.getWorld()->addObject(std::unique_ptr<Entity>(zone));
+}
+
+Engine::MapLoader::Zone::ZoneObject::ZoneObject(const std::string &libname, const Vector2d &pos) : _libname(libname),
+																								   _pos(pos)
 {
 }
 
